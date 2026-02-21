41086f55bfc7afa9e2fe9387706d12fc505c7383
mariaparis2000 <maria.paris@alumni.esade.edu>
2026-02-21 20:19:35 +0100

    browser tab

--- skill_gap.py
+++ skill_gap.py
@@ -46,3 +46,6 @@ if c2.button("🚀 Start match analysis", use_container_width=True):
         st.success("Analysis complete!")
     else:
         st.error("Missing data: Please provide both your profile and the job description.")
+
+#Browser tap customization:
+st.set_page_config(page_title="Skill-gap analyzer", layout="wide")

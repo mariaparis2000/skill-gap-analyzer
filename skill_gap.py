f25e696434e2b5a6317260ea6a01c0e786b36e81
mariaparis2000 <maria.paris@alumni.esade.edu>
2026-02-21 20:16:56 +0100

    body

--- skill_gap.py
+++ skill_gap.py
@@ -19,3 +19,30 @@ st.sidebar.info("1. Upload your CV\n2. Paste the job description\n3. Click Analy
 st.title("Skill-gap analyzer")
 st.markdown("Compare your professional profile against market requirements in real-time.")
 st.divider()
+
+#Body:
+col1, col2 = st.columns(2)
+
+col1.header("📂 Your profile")
+uploaded_cv = col1.file_uploader("Upload your CV (PDF)", type="pdf")
+
+col2.header("💼 Your target job")
+job_title = col2.text_input("Job title:", placeholder="e.g., Senior Data Analyst")
+job_desc = col2.text_area("Paste the job requirements here:", height=200)
+
+st.divider()
+
+c1, c2, c3 = st.columns([1, 1, 1])
+
+if c2.button("🚀 Start match analysis", use_container_width=True):
+    if (uploaded_cv) and job_desc:
+        progress_bar = st.progress(0)
+        st.info("Analyzing compatibility and identifying skill gaps...")
+        
+        for percent_complete in range(100):
+            time.sleep(0.01)
+            progress_bar.progress(percent_complete + 1)
+        
+        st.success("Analysis complete!")
+    else:
+        st.error("Missing data: Please provide both your profile and the job description.")

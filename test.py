d5786619db83bf5aab0dfc02b66060021e9216dc
mariaparis2000 <maria.paris@alumni.esade.edu>
2026-02-22 14:46:26 +0100

    Remove test.py with exposed key

--- test.py
+++
@@ -1,7 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyB7naAM-RU-tqUJ31jYAIm7t27VsSD-Bvo")
-
-for m in genai.list_models():
-    if "generateContent" in m.supported_generation_methods:
-        print(m.name)
